f1bac85dbf8ba203a37076b70cd7fd252700736b
Michael Zhao <michaeloz@Michaels-MBP-51.attlocal.net>
2020-04-08 18:57:48 -0700

    upload pics

--- init1.py
+++ init1.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template, request, session, url_for, redirect
 import pymysql.cursors
 import hashlib
 import datetime
+import os
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 #Initialize the app from Flask
 app = Flask(__name__)
+
 
 #Configure MySQL
 conn = pymysql.connect(host='localhost',
@@ -92,32 +96,40 @@ def registerAuth():
 
 @app.route('/home')
 def home():
+    if 'username' not in session:
+        return redirect(url_for('login'))
     user = session['username']
     cursor = conn.cursor()
-    query = 'SELECT postingDate, pID FROM Photo WHERE (poster = %s OR allFollowers = %s) ORDER BY postingDate DESC'
+    query = 'SELECT postingDate, pID, filePath FROM Photo WHERE (poster = %s OR allFollowers = %s) ORDER BY postingDate DESC'
     cursor.execute(query, (user, 1))
     data = cursor.fetchall()
     cursor.close()
+    disp_image(data)
     return render_template('home.html', username=user, posts=data)
 
 @app.route('/profile')
 def profile():
+    if 'username' not in session:
+        return redirect(url_for('login'))
     user = session['username']
     cursor = conn.cursor()
-    query = 'SELECT postingDate, pID FROM Photo WHERE poster = %s ORDER BY postingDate DESC'
+    query = 'SELECT postingDate, pID, filePath FROM Photo WHERE poster = %s ORDER BY postingDate DESC'
     cursor.execute(query, (user))
     data = cursor.fetchall()
     cursor.close()
+    disp_image(data)
     return render_template('profile.html', username=user, posts=data)
     
 @app.route('/photoDetail', methods=['GET', 'POST'])
 def photoDetail():
+    if 'username' not in session:
+        return redirect(url_for('login'))
     photo = request.args.get('pID')
-    print(photo)
     cursor = conn.cursor()
     query1 = 'SELECT * FROM Photo JOIN Person ON (username = poster) WHERE pID = %s'
     cursor.execute(query1, (photo))
     data1 = cursor.fetchall()
+    disp_image(data1)
     query2 = 'SELECT * FROM Tag JOIN Person USING (username) WHERE (pID = %s AND tagStatus = 1)'
     cursor.execute(query2, (photo))
     data2 = cursor.fetchall()
@@ -129,16 +141,33 @@ def photoDetail():
         
 @app.route('/post', methods=['GET', 'POST'])
 def post():
-    username = session['username']
-    cursor = conn.cursor()
-    postingDate = datetime.datetime.today()
-    photoPath = request.form['filePath'] #filePath is text
-    allFollowers = request.form['audiance'] #returns 1 if public, 0 if private. more values to be add later
-    caption = request.form['caption'] #caption is text
-    query = 'INSERT INTO Photo (postingDate, filePath, allFollowers, caption, poster) VALUES(%s, %s, %s, %s, %s)'
-    cursor.execute(query, (postingDate, photoPath, allFollowers, caption, username))
-    conn.commit()
-    cursor.close()
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    if request.method == 'POST':
+        # check if the post request has the file part
+        print(request.files)
+        if 'photo' not in request.files:
+            print('No file part')
+            return redirect(url_for('home'))
+        photoPath = request.files['photo']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if photoPath.filename == '':
+            print('No selected file')
+            return redirect(url_for('home'))
+        if photoPath and not allowed_file(photoPath.filename):
+            print('File type not allowed')
+            return redirect(url_for('home'))
+        username = session['username']
+        cursor = conn.cursor()
+        photoBLOB = convertToBinaryData(photoPath)
+        postingDate = datetime.datetime.today()
+        allFollowers = check_followers(request.form['audiance']) #returns 1 if public, 0 if private
+        caption = request.form['caption'] #caption is text
+        query = 'INSERT INTO Photo (postingDate,filePath, allFollowers, caption, poster) VALUES(%s, %s, %s, %s, %s)'
+        cursor.execute(query, (postingDate, photoBLOB, allFollowers, caption, username))
+        conn.commit()
+        cursor.close()
     return redirect(url_for('profile'))
 
 @app.route('/select_blogger')
@@ -169,11 +198,47 @@ def logout():
     session.pop('username')
     return redirect('/')
 
+
+''' Supplements used for Flask implementation '''
 def getHashed(password): 
     salt = "Finstagram"
     saltedPswd = password + salt
     pHash = hashlib.sha256(saltedPswd.encode()).hexdigest()
     return pHash
+
+### BLOB related implementations ###
+def convertToBinaryData(file):
+    # Convert digital data to binary format
+    binaryData = file.read()
+    return binaryData
+
+def disp_image(data):
+    folderPath = './static/user_uploads/'
+    if not os.path.exists(folderPath):
+        os.makedirs(folderPath)
+    for item in data:
+        image = item['filePath']
+        filePath = folderPath + str(item['pID'])
+        write_file(image, filePath)
+    files = []
+    with os.scandir(folderPath) as directory:
+        for item in directory:
+            if item.name[0] != '.':
+                files.append(folderPath + '/' + str(item.name))
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def check_followers(item):
+    if(item == 0 or item == 1):
+        return item
+    return 0
+
+def write_file(data, filename):
+    # Convert binary data to proper format and write it on Hard Disk
+    with open(filename, 'wb') as file:
+        file.write(data)
 
 app.secret_key = 'some key that you will never guess'
 #Run the app on localhost port 5000
